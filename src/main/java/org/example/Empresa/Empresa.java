8a5346597b1751a8992dcd9ba88984897bd709b0
Mateo <mateocarballo2@gmail.com>
2023-06-13 17:22:23 +0200

    Modificado a hashMap las listas de Empresas en Dialog y Producto en Empresas

--- src/main/java/org/example/Empresa/Empresa.java
+++ src/main/java/org/example/Empresa/Empresa.java
@@ -6,22 +6,23 @@ import org.example.Producto.ProductoAlquiler;
 import org.example.Producto.ProductoVenta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Empresa {
     private String cif;
     private String nombreEmpresa;
     private String telefono;
 
-    private ArrayList<Producto> listadoProductos;
-
+    private HashMap<String,Producto> hashMapProductosPorID;
     public Empresa(){
     }
 
-    public Empresa(String cif, String nombreEmpresa, String telefono) {
+    public Empresa(String cif, String nombreEmpresa, String telefono, HashMap<String, Producto> hashMapProductosPorID) {
         this.cif = cif;
         this.nombreEmpresa = nombreEmpresa;
         this.telefono = telefono;
-        this.listadoProductos=new ArrayList<>();
+        this.hashMapProductosPorID = new HashMap<>();
     }
 
     public String getCif() {
@@ -48,45 +49,69 @@ public class Empresa {
         this.telefono = telefono;
     }
     public void anadirProducto(ProductoVenta productoVenta){
-        this.listadoProductos.add(productoVenta);
+        this.hashMapProductosPorID.put(productoVenta.getCodigo(),productoVenta);
     }
     public void anadirProducto(ProductoAlquiler productoAlquiler){
-        this.listadoProductos.add(productoAlquiler);
+        this.hashMapProductosPorID.put(productoAlquiler.getCodigo(),productoAlquiler);
     }
 
     public void borrarProducto(String codigoProducto){
-        for (int i = 0; i < listadoProductos.size(); i++) {
-            if(listadoProductos.get(i).getCodigo().equals(codigoProducto)){
-                listadoProductos.remove(i);
-            }
-        }
+        hashMapProductosPorID.remove(codigoProducto);
     }
+
     public void modificarPrecioProducto(String codigoProducto,float nuevoValor){
-        for (Producto listadoProducto : listadoProductos) {
-            if ((listadoProducto instanceof ProductoVenta) &&
-                    (listadoProducto.getCodigo().equals(codigoProducto))) {
-                ((ProductoVenta) listadoProducto).setPrecioventa(nuevoValor);
-            }
-            if ((listadoProducto instanceof ProductoAlquiler) &&
-                    (listadoProducto.getCodigo().equals(codigoProducto))) {
-                ((ProductoAlquiler) listadoProducto).setPrecioDia(nuevoValor);
+        for (Map.Entry<String, Producto> entry : hashMapProductosPorID.entrySet()) {
+            Producto producto = entry.getValue();
+            String key = entry.getKey();
+            /* No me queda muy claro si estoy modificando los elementos del hashMap o no. Entiendo que si porque
+                se pasan como referencia y no como valor pero me gustaria una explicacion. No me queda claro
+                 */
+            if (key.equalsIgnoreCase(codigoProducto)){
+                if (producto instanceof ProductoAlquiler alquiler) {
+                    alquiler.setPrecioDia(nuevoValor);
+
+                } else if (producto instanceof ProductoVenta venta) {
+                    venta.setPrecioventa(nuevoValor);
+                }
             }
         }
     }
+
+    /* CODIGO ANTES DEL CAMBIO A HASHMAPS
+    for (Producto p:listadoProductos){
+            if ((p.getCodigo().equalsIgnoreCase(codigoProductoAlquiler)) && (((ProductoAlquiler) p).getEstado() == 'L')) {
+                            break;
+            }
+        }
+
+        public void modificarPrecioProducto(String codigoProducto,float nuevoValor){
+
+            for (Producto listadoProducto : listadoProductos) {
+                if ((listadoProducto instanceof ProductoVenta) &&
+                        (listadoProducto.getCodigo().equals(codigoProducto))) {
+                    ((ProductoVenta) listadoProducto).setPrecioventa(nuevoValor);
+                }
+                if ((listadoProducto instanceof ProductoAlquiler) &&
+                        (listadoProducto.getCodigo().equals(codigoProducto))) {
+                    ((ProductoAlquiler) listadoProducto).setPrecioDia(nuevoValor);
+                }
+            }
+
+    }
+     */
     public boolean comprobarDisponibilidad(String codigoProductoAlquiler){
         boolean disponible =false;
-        for (Producto p:listadoProductos){
-            if ((p.getCodigo().equalsIgnoreCase(codigoProductoAlquiler)) && (((ProductoAlquiler) p).getEstado() == 'L')) {
-                disponible = true;
-                // TODO esto se deberia quitar??
-                break;
+            for (Producto p : hashMapProductosPorID.values()){
+                if ((p.getCodigo().equalsIgnoreCase(codigoProductoAlquiler)) && (((ProductoAlquiler) p).getEstado() == 'L')) {
+                    disponible = true;
+                    break;
+                }
             }
+        return disponible;
         }
-       return disponible;
-    }
+
     public void alquilarProducto(String codigoProducto) throws ArticuloAlquilado {
         if (comprobarDisponibilidad(codigoProducto)){
-
         }else{
             throw new ArticuloAlquilado();
         }
